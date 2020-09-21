f13e837b493832cac46a1677a23c17964d43b44b
Khash Sajadi <khash@sajadi.co.uk>
2020-09-21 14:28:33 -0700

    support for timeout

--- main.go
+++ main.go
@@ -9,11 +9,25 @@ import (
 	"time"
 )
 
+var defaultTimeout time.Duration
+
 func main() {
 	region := os.Getenv("REGION")
 	if region == "" {
 		log.Fatal("no region set")
 	}
+
+	defaultTimeout = 100*time.Millisecond
+	var err error
+	timeoutDuration := os.Getenv("TIMEOUT")
+	if timeoutDuration != "" {
+		defaultTimeout, err = time.ParseDuration(timeoutDuration)
+		if err != nil {
+			log.Fatalf("invalid default timeout %s", err.Error())
+		}
+	}
+
+	log.Printf("using %s as default timeout", defaultTimeout)
 
 	log.Printf("starting watchman region %s...", region)
 	http.HandleFunc("/", handler)
@@ -38,23 +52,37 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return 
 	}
-
 	url := urls[0]
 
-	log.Printf("checking %s...", url)
+	var err error 
+	timeout := defaultTimeout
+	customTimeouts, ok := query["timeout"]
+	if ok && len(customTimeouts) != 0 {
+		timeout, err = time.ParseDuration(customTimeouts[0])
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Print("bad timeout")
+			w.Write([]byte("bad timeout"))
+			return 
+		}
+	}
+
+	log.Printf("checking %s... (timeout: %s)", url, timeout)
 	ctx := context.Background()
-	err := check(ctx, url)
+	err = check(ctx, url, timeout)
 	if err != nil {
+		log.Print(err.Error())
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(err.Error()))
 	} else {
 		w.WriteHeader(http.StatusOK)
+		log.Print("success")
 		w.Write([]byte("ok"))
 	}
 }
 
-func check(ctx context.Context, url string) error {
-	ctx, cancel := context.WithTimeout(ctx, 100*time.Millisecond)
+func check(ctx context.Context, url string, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
 	req, err := http.NewRequest("GET", url, nil)
